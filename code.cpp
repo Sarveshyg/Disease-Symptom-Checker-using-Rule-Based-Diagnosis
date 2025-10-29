2ef7381eb729873fe381ff8d0075c52a95e9c7bb
Sarvesh Garude <garudeysarvesh@gmail.com>
2025-10-29 17:39:25 +0530

    feat: implement disease rule loading and user symptom input
    - Added trim() utility for whitespace cleanup
    - Implemented gather_disease() to load disease–symptom mappings from rules.txt
    - Implemented get_user_symptoms() to take symptom input interactively from the user
    - Added function placeholders for diagnose() to be implemented later
    - Set up main() to gather rules, collect user symptoms, and prepare for diagnosis logic

---
+++ code.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <bits/stdc++.h>
+#include <sstream>
+using namespace std;
+
+string trim(const string &str)
+{
+    size_t start = str.find_first_not_of(" \t");
+    size_t end = str.find_last_not_of(" \t");
+    if (start == string::npos)
+        return "";
+    return str.substr(start, end - start + 1);
+}
+
+void gather_disease(map<string, set<string>> &diseaseRules)
+{
+    fstream rule("rules.txt", ios::in);
+    string data;
+    if (!rule.is_open())
+    {
+        cerr << "Failed to open the file. Please check if it exists.";
+        return;
+    }
+    while (getline(rule, data))
+    {
+        if (data.empty())
+            continue;
+
+        size_t colonPos = data.find(':');
+        if (colonPos == string::npos)
+            continue;
+
+        string diseaseName = trim(data.substr(0, colonPos));
+        string diseaseSymptoms = trim(data.substr(colonPos + 1));
+
+        stringstream ss(diseaseSymptoms);
+        string individualSymptom;
+        while (getline(ss, individualSymptom, ','))
+        {
+            individualSymptom = trim(individualSymptom);
+            diseaseRules[diseaseName].insert(individualSymptom);
+        }
+    }
+}
+
+set<string> get_user_symptoms()
+{
+    set<string> userDisease;
+    string userInput;
+
+    do
+    {
+        cout << "Enter your symptoms one at a time: ";
+        getline(cin, userInput);
+        if(userInput == "done" || userInput.empty()){
+            break;
+        }
+        userInput = trim(userInput);
+        userDisease.insert(userInput);
+    } while (true);
+
+
+    return userDisease;
+}
+
+set<string> diagnose(map<string, set<string>>& diseaseRules, set<string>& userSymptoms){
+    set<string> possibleDiseases;
+}
+
+int main()
+{
+    map<string, set<string>> diseaseRules;
+    gather_disease(diseaseRules);
+
+    set<string> userSymptoms = get_user_symptoms();
+
+    set<string> userDisease = diagnose(diseaseRules, userSymptoms);
+
+    return 0;
+}
